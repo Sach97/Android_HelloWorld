7da606725a70621947e745a713adb5d2f1600325
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2017-09-14 17:22:39 +0200

    hello world

--- app/src/main/java/com/example/sacha/helloworld/MainActivity.java
+++ app/src/main/java/com/example/sacha/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sacha.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //For debugging
+        Toast.makeText(this,"OnCreate",Toast.LENGTH_SHORT).show();
+        //Modifier l'icone de l'application
+        //Modifier le layout pour que le texte soit centré et de couleur différente
     }
 }
